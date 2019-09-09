de67aef1e8023bc6d5934997a6beb331fed34408
Phil Dubach <dubachphil@hotmail.com>
2019-09-09 22:59:19 +0200

    div on cltrace.py

--- cltrace.py
+++ cltrace.py
@@ -6,13 +6,24 @@ class mrWordCount(MRJob):
 
 
     def mapper(self, _, line):
+       words = line.split(',')
        if "time" not in line: 
-          words = line.split(',')
-          yield words[1] + "/" + words[2],int(words[6])
+          yield words[1] + "/" + words[2], int(words[6])
+       if "msg_snd" in line:
+          yield words[1] + "/" + words[2], ((int(words[6])//1000)*60) 
 
     def reducer(self, key, values):
-       
-       yield key, np.diff(values)
+       values = list(values)
+       if len(values) == 3:
+        yield values[1], np.mean(values[2]-values[0])
+
+
+#         yield key, values[0]
+
+#    def reducer(self, key, values):
+#        yield key, count(values)
+
+
 
 if __name__ == '__main__':
     mrWordCount.run()
